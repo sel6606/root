dd124290d50fb0730edc0e929e516ed700fb0dc6
Austin Duvin <axd6329@g.rit.edu>
2016-03-12 15:33:08 -0500

    no message

--- ROOT/UIMan.cs
+++ ROOT/UIMan.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace ROOT
 {
     class UIMan
     {
-        public UIMan() { }
+        Game1 game;
+        SpriteFont UIFont;
 
-        public void Draw() { }
+        public UIMan(Game1 game, SpriteFont UIFont)
+        {
+            this.game = game;
+            this.UIFont = UIFont;
+        }
+
+        public void Draw(SpriteBatch s)
+        {
+            s.DrawString(UIFont, String.Format("{0:0.00}", game.Timer1), new Vector2((game.GraphicsDevice.Viewport.Width / 2) - 20, 50), Color.White);
+            s.DrawString(UIFont, String.Format("{0:0.00}", game.Timer2), new Vector2((game.GraphicsDevice.Viewport.Width / 2) + 20, 50), Color.White);
+        }
     }
 }
